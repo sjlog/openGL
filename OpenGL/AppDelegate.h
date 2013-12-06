a3e4ef603621e86c9d298e5fc922f0a78cadefe5
sangjo_itwill <sangjo_itwill@local.local>
2013-12-06 14:20:49 +0900

    Initial Commit

---
+++ OpenGL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGL
+//
+//  Created by sangjo_itwill on 2013. 12. 6..
+//  Copyright (c) 2013년 sj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenGL
+//
+//  Created by sangjo_itwill on 2013. 12. 6..
+//  Copyright (c) 2013년 sj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
